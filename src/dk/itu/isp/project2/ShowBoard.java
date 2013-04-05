c0dd4185196d248012f0559ed2744a5f81d68610
Joana <test@test.com>
2013-04-05 16:00:22 +0200

    use argument from user input
    user can determine how big the game board should be

--- src/dk/itu/isp/project2/ShowBoard.java
+++ src/dk/itu/isp/project2/ShowBoard.java
@@ -7,20 +7,27 @@ package dk.itu.isp.project2;
 import javax.swing.*;
 
 public class ShowBoard {
-	public static void main(String[] arg) {
-		QueensLogic l = new QueensLogic();
-		
-		int size = 5;
-		l.initializeGame(size);
 
-		QueensGUI g = new QueensGUI(l);
+    public static void main(String[] arg) {
+        QueensLogic l = new QueensLogic();
+        int size = 5;
+        if (arg.length == 1) {
+            try {
+                size = Integer.valueOf(arg[0]);
+            } catch (Exception e) {
+                System.out.println("Default game board config: 5 * 5");
+            }
+        }
+        l.initializeGame(size);
 
-		// Setup of the frame containing the game
-		JFrame f = new JFrame();
-		f.setSize(200 + size * 100, 200 + size * 100);
-		f.setTitle("5 Queens Game");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.getContentPane().add(g);
-		f.setVisible(true);
-	}
+        QueensGUI g = new QueensGUI(l);
+
+        // Setup of the frame containing the game
+        JFrame f = new JFrame();
+        f.setSize(200 + size * 100, 200 + size * 100);
+        f.setTitle(size + " Queens Game");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(g);
+        f.setVisible(true);
+    }
 }
